360560bea32ea8e3c23bbf71cd60eeb6f68d9794
Semi <94402613+s-em-i@users.noreply.github.com>
2025-09-21 20:59:56 +0200

    Update tguploadbot.py

--- .github/workflows/tguploadbot.py
+++ .github/workflows/tguploadbot.py
@@ -1,45 +1,43 @@
 import os
 import sys
 import asyncio
-from telethon import TelegramClient
+from telethon import TelegramClient, utils
 from telethon.sessions import StringSession
+from telethon.tl.types import PeerChannel
+from telethon.tl import types
 
 API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 SESSION = os.environ.get("SESSION")
-CHAT_ID = os.environ.get("CHAT_ID")
-MESSAGE_THREAD_ID = os.environ.get("MESSAGE_THREAD_ID")
+CHAT_ID = int(os.getenv("CHAT_ID"))
+MESSAGE_THREAD_ID = int(os.getenv("MESSAGE_THREAD_ID"))
 COMMIT_MESSAGE = os.environ.get("COMMIT_MESSAGE")
 
 def create_client():
     return TelegramClient(StringSession(SESSION), API_ID, API_HASH)
-
-def get_caption():
-    return COMMIT_MESSAGE + "\n"
 
 async def main():
     if not all([API_ID, API_HASH, SESSION, CHAT_ID]):
         print("[-] Missing required env vars!")
         sys.exit(1)
     
-    print("[+] Starting Telethon album upload")
+    print("[+] Starting Telethon upload")
     print("[+] Files:", sys.argv[1:])
-    
-    client = create_client()
 
+    client = create_client()
+    
     async with client:
         try:
-            entity = await client.get_entity(CHAT_ID)
+            real_id, peer_type = utils.resolve_id(CHAT_ID)        
+            entity = await client.get_entity(PeerChannel(real_id))
+            print("[+] Entity Found") 
         except Exception as e:
             print(f"[-] Failed to resolve chat entity: {e}")
             sys.exit(1)
 
         paths = sys.argv[1:]
-        caption = get_caption()
         
-        print("[+] Caption:")
-        print(caption)
-        print("---")
+        print(f"[+] Caption: {COMMIT_MESSAGE}")
         
         valid_files = []
         for one in paths:
@@ -51,22 +49,22 @@ async def main():
         
         if not valid_files:
             print("[-] No valid files to upload!")
-            return
-        
-        print(f"[+] Uploading {len(valid_files)} files as album...")
+            return       
         
         if len(valid_files) == 1:
+            print(f"[+] Uploading {len(valid_files)} file(")
             await client.send_file(
-                CHAT_ID,
+                entity,
                 valid_files[0],
-                caption=caption,
+                caption=COMMIT_MESSAGE,
                 reply_to=MESSAGE_THREAD_ID
             )
         else:
+            print(f"[+] Uploading {len(valid_files)} files as album...")
             await client.send_file(
-                CHAT_ID,
+                entity,
                 valid_files,
-                caption=caption,
+                caption=COMMIT_MESSAGE,
                 reply_to=MESSAGE_THREAD_ID
             )
         
