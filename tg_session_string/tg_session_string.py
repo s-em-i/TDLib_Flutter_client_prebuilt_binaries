50a7a082cdf59ae4e0f5a6fbc8c1b2cdde4202de
semi <s-em-i@github.com>
2025-09-27 21:11:58 +0200

    added tg session string script

---
+++ tg_session_string/tg_session_string.py
@@ -0,0 +1,24 @@
+import telethon, sys
+from telethon.sync import TelegramClient
+from telethon.sessions import StringSession
+from telethon import TelegramClient
+from telethon.sessions import StringSession
+
+async def main(client: telethon.TelegramClient):
+
+    if(not client.is_connected()):
+        await client.connect()
+    
+    session_string = client.session.save()
+    print(f"\nSESSION STRING (for GitHub Secrets):")
+    print(session_string)
+
+###### Script Entrypoint ######
+
+api_id = ""
+api_hash = ""
+
+client = TelegramClient(StringSession(), api_id, api_hash)
+
+with client:
+   client.loop.run_until_complete(main(client))
